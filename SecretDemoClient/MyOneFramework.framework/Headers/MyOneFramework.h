b7ee263438a52000e72550552f699b85d25ebeb8
Jak Ratiwanich <jratiwanich@idemia-nss.com>
2021-08-05 17:57:47 -0700

    first commit

---
+++ SecretDemoClient/MyOneFramework.framework/Headers/MyOneFramework.h
@@ -0,0 +1,18 @@
+//
+//  MyOneFramework.h
+//  MyOneFramework
+//
+//  Created by Jak Ratiwanich on 8/5/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyOneFramework.
+FOUNDATION_EXPORT double MyOneFrameworkVersionNumber;
+
+//! Project version string for MyOneFramework.
+FOUNDATION_EXPORT const unsigned char MyOneFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyOneFramework/PublicHeader.h>
+
+
--- SecretDemoClient/ViewController.swift
+++ SecretDemoClient/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import MyOneFramework
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clickMe: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+ 
+        
     }
 
-
+    @IBAction func clickShow(_ sender: Any) {
+        let manager = SecretManager(key: "12345")
+        let message = manager.readSecret()
+        print(message)
+    }
+    
 }
+
 
